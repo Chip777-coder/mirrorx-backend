106b86def1f1f895137b37b362e335b02538af61
Chip777-coder <juslakers@gmail.com>
2026-01-18 15:01:35 -0500

    Create weather.py

---
+++ src/utils/weather.py
@@ -0,0 +1,8 @@
+def get_weather_for_game(location):
+    # TODO: Integrate real weather API
+    return {
+        "wind": "12 mph W",
+        "temp": "78°F",
+        "precip": "0%",
+        "comment": "No impact expected"
+    }
