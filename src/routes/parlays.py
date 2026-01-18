57e6973021c3c7281b0a6776a67b90375f665c1f
Chip777-coder <juslakers@gmail.com>
2026-01-18 15:01:01 -0500

    Update parlays.py

--- src/routes/parlays.py
+++ src/routes/parlays.py
@@ -1,10 +1,8 @@
-# parlays.py
 from flask import Blueprint, jsonify
-from analytics.mirrax.parlay_builder import build_parlay
+from analytics.mirrax.parlay_builder import generate_multiple_parlays
 
-parlays_bp = Blueprint('parlays', __name__)
+parlays_bp = Blueprint("parlays", __name__)
 
 @parlays_bp.route("/parlay/today", methods=["GET"])
-def get_today_parlay():
-    parlay = build_parlay()
-    return jsonify(parlay)
+def get_parlays():
+    return jsonify(generate_multiple_parlays())
